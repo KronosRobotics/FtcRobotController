bdbb941742f4f0d2ac8f8fb717b18e888384f490
Kshitij T <teotia.kshitij@gmail.com>
2020-10-21 11:31:07 -0700

    Encoder Practice

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -9,6 +9,8 @@ public class Robot {
     //2 left motors 2 right motors
     public HardwareMap hwMap = null;
     public void init(HardwareMap hardwareMap){
+        //encoder measures the distance we are going
+
         hwMap = hardwareMap;
         l1 = hwMap.get(DcMotor.class, "l1");
         l2 = hwMap.get(DcMotor.class, "l2");
@@ -18,6 +20,13 @@ public class Robot {
         l2.setDirection(DcMotor.Direction.FORWARD);
         r1.setDirection(DcMotor.Direction.REVERSE);
         r2.setDirection(DcMotor.Direction.REVERSE);
+        //use the encoders for the motors
+        //helps us us the encoders for the motors
+        l1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        r1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        l2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        r2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //encoder measures how much the motor moves
         //motorname.setpower(power val(double in the range of 0 - 1 inclusive));
         l1.setPower(0);
         l2.setPower(0);
@@ -32,5 +41,16 @@ public class Robot {
         l2.setPower(power - turn);
     }
 
-
+    public void moveDis(double power, int dis){
+        r1.setTargetPosition(dis);
+        r2.setTargetPosition(dis);
+        r1.setPower(1);
+        r1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        r2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        while(r1.isBusy()){}
+        r1.setPower(0);
+        r2.setPower(0);
+        r1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        r2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
 }
